8deab4047d2eb725542b9722f1ef6fe5966d0112
jjy <jjyruby@gmail.com>
2025-07-07 00:06:51 +0800

    Add i18n config

--- flake.nix
+++ flake.nix
@@ -36,5 +36,13 @@
           ./users/jjy/home.nix
       ];
     };
+
+    i18n.inputMethod = {
+      enable = true;
+      type = "fcitx5";
+      fcitx5.addons = with pkgs; [
+        fcitx5-hangul
+      ];
+    };
   };
 }
