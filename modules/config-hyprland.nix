853f921c35b3da0206b11a042743fa471af33bf8
jjy <jjyruby@gmail.com>
2025-07-08 00:27:21 +0800

    Resolve confcliting

--- modules/config-hyprland.nix
+++ modules/config-hyprland.nix
@@ -8,7 +8,6 @@
 {
   wayland.windowManager.hyprland = {
     enable = true;
-    package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
 
     systemd = {enable = true; variables = ["--all"];};
     xwayland.enable = true;
