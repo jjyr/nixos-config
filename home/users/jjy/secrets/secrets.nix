77c5caab5a2f0020ed6e45dd620101733685788b
jjy <jjyruby@gmail.com>
2025-07-08 18:54:24 +0800

    Add laptop key to secrets

--- home/users/jjy/secrets/secrets.nix
+++ home/users/jjy/secrets/secrets.nix
@@ -1,6 +1,9 @@
 let
   users = [
+    # homepc
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDXl8P73ghkshgCsqRGphdrEKkkxxEfz4ug+TiqY7uLR"
+    # laptop
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINzqqIUmjCCZSh6+5xYBtTMQpK1FSA36IHTaWC6qt+jG"
   ];
 
 in
