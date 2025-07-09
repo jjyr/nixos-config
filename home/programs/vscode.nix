7a333640c3821f7038de3521cade9337f04ea959
jjy <jjyruby@gmail.com>
2025-07-09 21:28:00 +0800

    forbid ssh password; fix nvidia; use vscode bin

--- home/programs/vscode.nix
+++ home/programs/vscode.nix
@@ -8,7 +8,6 @@
 {
   programs.vscode = {
     enable = true;
-    package = pkgs.vscodium;
     profiles.default = {
       extensions = with pkgs.vscode-extensions; [
         rust-lang.rust-analyzer
--- machines/homepc/default.nix
+++ machines/homepc/default.nix
@@ -6,13 +6,21 @@
     ../common.nix
   ];
 
-  services.openssh.enable = true;
+  services.openssh = {
+    enable = true;
+    settings = {
+      PasswordAuthentication = false;
+    };
+  };
 
   users.users.root.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINzqqIUmjCCZSh6+5xYBtTMQpK1FSA36IHTaWC6qt+jG"
   ];
 
   # nvidia
+  environment.systemPakcage = with pkgs; [
+    egl-wayland
+  ];
   nixpkgs.config.allowUnfree = true;
 
   services.xserver.videoDrivers = [ "nvidia" ];
@@ -20,7 +28,15 @@
     enable = true;
     enable32Bit = true;
   };
-  hardware.nvidia.open = true;
+  hardware.nvidia = {
+    modesetting.enable = true;
+    powerManagement = {
+      enable = true;
+      finegrained = false;
+    };
+    open = true;
+    nvidiaSettings = true;
+  };
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
